a6a80bb486fcb3ac2615d632110319d9d8826aed
nima <n.mousavi@aban.io>
2020-08-16 17:41:58 +0430

    add constructor for general static

--- src/main/java/ir/webold/framework/config/general/GeneralStatic.java
+++ src/main/java/ir/webold/framework/config/general/GeneralStatic.java
@@ -1,6 +1,11 @@
 package ir.webold.framework.config.general;
 
 public class GeneralStatic {
+
+    private GeneralStatic() {
+        throw new IllegalStateException("GeneralStatic class");
+    }
+
     public static final String RRN = "rrn";
     public static final String AUTHORIZATION = "Authorization";
     public static final String BASIC = "Basic ";
