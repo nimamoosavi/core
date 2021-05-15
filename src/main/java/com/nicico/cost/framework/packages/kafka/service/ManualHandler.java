9990d616a09fb075592fda6b71d05c7d86445627
nima <nmoosavi@live.com>
2021-05-15 13:29:07 +0430

    KafkaConsumer Added ManualHandler

---
+++ src/main/java/com/nicico/cost/framework/packages/kafka/service/ManualHandler.java
@@ -0,0 +1,11 @@
+package com.nicico.cost.framework.packages.kafka.service;
+
+/**
+ * @author nima
+ * @version 1.0.1
+ * @implNote this class has an interface class {@link com.nicico.cost.kafka.service.ManualHandler} that you must implement handler method
+ * you can see example of it in {@link com.nicico.cost.kafka.service.manual.ExampleHandler}
+ * and you must set kafka.manual.handle properties true for manual handle .. default is true
+ */
+public interface ManualHandler {
+}
