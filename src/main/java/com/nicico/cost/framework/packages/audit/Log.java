c5ba4ae0930382aaacf8641627c0dd2236fa60e0
nima <nmoosavi@live.com>
2021-05-15 14:22:14 +0430

    add KAFKA to audit

--- src/main/java/com/nicico/cost/framework/packages/audit/Log.java
+++ src/main/java/com/nicico/cost/framework/packages/audit/Log.java
@@ -5,7 +5,7 @@ import com.nicico.cost.framework.packages.audit.view.AuditFactory;
 /**
  * @version 1.0.1
  * @apiNote you can use AuditFactory class for create AuditVM
- * this implementation has some locations in AuditServiceImpl and you can set Kafka or File to config
+ * this implementation has some locations in AuditServiceImpl and you can set Kafka or File {@link com.nicico.cost.audit.enums.Locations}
  * @author nima mousavi
  */
 public interface Log {
