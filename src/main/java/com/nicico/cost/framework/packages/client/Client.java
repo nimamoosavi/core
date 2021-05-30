141bb094034f5384da8b7916a2a2713df0c5b281
nima <nmoosavi@live.com>
2021-05-30 14:30:29 +0430

    Client Library

---
+++ src/main/java/com/nicico/cost/framework/packages/client/Client.java
@@ -0,0 +1,13 @@
+package com.nicico.cost.framework.packages.client;
+
+
+/**
+ * @author nima
+ * @version 1.0.1
+ * @implNote this class generate For Connect To Another Microservice . you can create your client Layer and used For Call Base Controller Api in CRUD Library
+ * by Default and Implement Another Microservices
+ * Good Time
+ * @since 1.0.1
+ */
+public interface Client {
+}
