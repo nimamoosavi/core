2d3cba6582cb2d18ccc15ae0822a0e34cb39b23c
nima <nmoosavi@live.com>
2021-06-12 14:34:23 +0430

    clean code

--- src/main/java/com/nicico/cost/framework/config/general/GeneralStatic.java
+++ src/main/java/com/nicico/cost/framework/config/general/GeneralStatic.java
@@ -5,28 +5,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GeneralStatic {
+    private GeneralStatic() {
+        throw new IllegalStateException("GeneralStatic class");
+    }
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String APP_KEY = "appKey";
     public static final String CORRELATION_ID = "correlationId";
     public static final String CLIENT_VERSION = "client-version";
-    public static String basic;
+    public static final String BASIC = "basic";
     public static String datePersianPatternWithDetail;
     public static String datePersianPattern;
     public static String applicationName;
     public static String instanceId;
     public static int connectionTimeOut;
     public static int readTimeOut;
-    private static String refreshToken;
-    private static String basicAuthentication;
-    private static String roles;
-    private static String userName;
-    private static String userId;
-    private static String requestId;
-    private static String swaggerTittle;
-    private static String swaggerDescription;
-    private static String swaggerLicence;
-    private static String swaggerVersion;
 
     @Value("${connection.timeout:5000}")
     public void setConnectionTimeOut(int connectionTimeOut) {
@@ -38,16 +31,6 @@ public class GeneralStatic {
         GeneralStatic.readTimeOut = readTimeOut;
     }
 
-    @Value("${core.refreshToken:refreshToken}")
-    public void setRefreshToken(String refreshToken) {
-        GeneralStatic.refreshToken = refreshToken;
-    }
-
-    @Value("${core.basic:Basic}")
-    public void setBasic(String basic) {
-        GeneralStatic.basic = basic;
-    }
-
     @Value("${core.datePersianPatternWithDetail:yyyy/MM/dd hh:mm}")
     public void setDatePersianPatternWithDetail(String datePersianPatternWithDetail) {
         GeneralStatic.datePersianPatternWithDetail = datePersianPatternWithDetail;
@@ -56,33 +39,6 @@ public class GeneralStatic {
     @Value("${core.datePersianPattern:yyyy/MM/dd}")
     public void setDatePersianPattern(String datePersianPattern) {
         GeneralStatic.datePersianPattern = datePersianPattern;
-    }
-
-    @Value("${core.basic:Basic}")
-    public void setBasicAuthentication(String basicAuthentication) {
-        GeneralStatic.basicAuthentication = basicAuthentication;
-    }
-
-    @Value("${spring.application.name}")
-    public void setApplicationName(String applicationName) {
-        GeneralStatic.applicationName = applicationName;
-    }
-
-    @Value("${core.roles:roles}")
-    public void setRoles(String roles) {
-        GeneralStatic.roles = roles;
-    }
-
-
-    @Value("${core.userId:userId}")
-    public void setUserId(String userId) {
-        GeneralStatic.userId = userId;
-    }
-
-
-    @Value("${core.requestId:requestId}")
-    public void setRequestId(String requestId) {
-        GeneralStatic.requestId = requestId;
     }
 
     @Value("${application.instanceId}")
